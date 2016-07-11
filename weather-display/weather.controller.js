ae2b76b30d24ba79dcda350c45fb0ea5fd0411d5
Jenni Bradstreet <jennic15@gmail.com>
2016-07-10 21:52:39 -0700

    Make sure files reference each other.

---
+++ weather-display/weather.controller.js
@@ -0,0 +1,19 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('weatherApp');
+    .controller('WeatherController', WeatherController)
+
+    // weatherService is the name of my factory-service
+    function WeatherController($scope, weatherService) {
+      $scope.vm = {};
+
+      weatherService.getData().then(function(data) {
+        $scope.vm.weatherData = data;
+      })
+
+
+    }
+
+})();
--- weather-display/weather.service.js
+++ weather-display/weather.service.js
@@ -1,6 +1,31 @@
 (function() {
   'use strict';
 
-  
+  angular
+    .module('weatherApp')
+    .factory('weatherService', weatherService)
 
-});
+    function weatherService($http, $q) {
+      var data = [];
+
+      return {
+
+        getData: function() {
+          $http.get('https://api.forecast.io/forecast/API_KEY/47.6062,122.3321')
+          .then(function(result) {
+            console.log(result);
+            data = result.data;
+            console.log(data);
+            return data;
+          })
+        } else {
+          return $q(function(resolve, reject) {
+            resolve(data);
+          });
+        }
+      }
+
+
+    }
+
+})();
