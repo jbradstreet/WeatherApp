f2c7a1616fb2bff7a2b165fe0d51895227cfc867
Jenni Bradstreet <jennic15@gmail.com>
2016-07-11 16:23:47 -0700

    Add a comment, return to this for full understanding.

--- weather-display/weather.controller.js
+++ weather-display/weather.controller.js
@@ -13,6 +13,7 @@
       var example = weatherService.getData();
       $scope.vm.data = example;
       console.log($scope);
+      // removed ".then", not utilizing a promise in my service file.
       // .then(function(data) {
       //   $scope.vm.data = data;
       // })
