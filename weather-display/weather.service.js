48d248357d6a6788c2f3f4c03c2805487a66797a
Jenni Bradstreet <jennic15@gmail.com>
2016-07-11 16:24:51 -0700

    Add code to overcome CORS error, use API key for now, hide in server side code later...hopefully.

--- weather-display/weather.service.js
+++ weather-display/weather.service.js
@@ -5,27 +5,31 @@
     .module('weatherApp')
     .factory('weatherService', weatherService)
 
+    weatherService.$inject = ['$http', '$q']
+
     function weatherService($http, $q) {
       var data = [];
 
       return {
-
         getData: function() {
-          $http.get('https://api.forecast.io/forecast/API_KEY/47.6062,122.3321')
-          .then(function(result) {
+          var deferred = $q.defer();
+          $http.jsonp('https://api.forecast.io/forecast/a379801180042a5b59fa3c0034b64131/47.6062,122.3321' + "?callback=JSON_CALLBACK")
+          .success(function(result) {
+            deferred.resolve(result);
             console.log(result);
             data = result.data;
-            console.log(data);
             return data;
-          })
-        // } else {
-        //   return $q(function(resolve, reject) {
-        //     resolve(data);
-        //   });
+          }, function(error) {
+            deferred.reject(error);
+          });
+          return deferred.promise;
+          }
         }
       }
-
-
-    }
-
 })();
+
+
+// } else {
+//   return $q(function(resolve, reject) {
+//     resolve(data);
+//   });
