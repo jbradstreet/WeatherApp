8d41901a9bf72f84c4c4abacf4d32a67ecb567e7
Jenni Bradstreet <jennic15@gmail.com>
2016-07-12 14:00:54 -0700

    Remove references to $scope.

--- weather-display/weather.controller.js
+++ weather-display/weather.controller.js
@@ -6,25 +6,15 @@
     .controller('WeatherController', WeatherController)
 
     // weatherService is the name of my factory-service
-    function WeatherController($scope, weatherService) {
-      $scope.vm = {};
-      $scope.vm.message = "Hello!";
+    function WeatherController(weatherService) {
+      var vm = this;
+      vm.message = "Hello!";
 
-      var data = weatherService.getData();
-        $scope.vm.data = data;
+       weatherService.getData().then(function(data) {
+        vm.data = data;
         console.log(data);
 
-
-      // $scope.vm.weatherIcon = {
-      //   access weatherService data
-      //   if ($scope.vm.data.$$state.value.daily[0].icon)
-      //   forecast: {
-      //     icon: "rain",
-      //     iconSize: 50,
-      //     color: "blue"
-      //   }
-      //   console.log($scope.vm.data);
-      // };
+      });
     }
 
 })();
